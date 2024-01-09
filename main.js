2b03f5bd1182e521686c0e44857579fae12d8f57
Afpa <alex.nesvit2000@gmail.com>
2024-01-09 10:58:26 +0100

    lesson4

---
+++ main.js
@@ -0,0 +1,7 @@
+//document.write("Hello world!"); text
+//console.log("Hello world!"); text
+//console.info("Hello world!"); info
+//console.error("Hello world!"); alert rouge
+//console.warn("Hello world!"); alert jeune
+
+var num;
