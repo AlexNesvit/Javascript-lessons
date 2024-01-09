426a78cd308b10ab47d34f3e35dda62f84ebafb0
Afpa <alex.nesvit2000@gmail.com>
2024-01-09 11:17:39 +0100

    lesson4 ok

--- main.js
+++ main.js
@@ -4,4 +4,14 @@
 //console.error("Hello world!"); alert rouge
 //console.warn("Hello world!"); alert jeune
 
-var num;
+//var num;
+//num = 5;
+const num = 5;
+//num = 7;
+console.log("Number: " + num + ".");
+
+var number;
+number = true;
+
+var num_1 = 5;
+var num_2 = "5";
