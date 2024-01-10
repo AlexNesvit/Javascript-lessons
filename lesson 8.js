e406a384c550bf45df1277a618f6c677684f0587
Afpa <alex.nesvit2000@gmail.com>
2024-01-10 12:07:33 +0100

    lesson 8

---
+++ lesson 8.js
@@ -0,0 +1,31 @@
+//   lesson 8 cikli
+/*
+for(var i = 100; i > 5; i /= 2) {
+    console.log(i);
+}
+*/
+/*
+var j = 1000;
+while(j >= 100) {
+    console.log(j);
+    j -= 100;
+}
+
+var isHasCar = true;    
+while(isHasCar) {
+    beskonechnii cikl warning
+}
+*/
+/*
+var x = 0;
+do {
+    console.log(x);
+    x ++;
+} while(x < 10);
+*/
+
+for(var i = 10; i <= 20; i += 2) {
+    //if(i > 15)
+   // break;
+    console.log(i);
+}
