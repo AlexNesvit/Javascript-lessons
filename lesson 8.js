767e697e7df58fd8bdc05a0525e110fdc4c26956
Afpa <alex.nesvit2000@gmail.com>
2024-01-17 11:05:06 +0100

    Lesson 8 ok

--- lesson 8.js
+++ lesson 8.js
@@ -23,10 +23,17 @@ do {
     x ++;
 } while(x < 10);
 */
-
-for(var i = 10; i <= 20; i += 2) {
+/*
+for(var i = 10; i <= 20; i ++) {
     if(i % 2 == 0)
     continue;
    // break;
     console.log(i);
+}
+*/
+
+var arr = [5, 7, 3, 8, 9, "stroka"];
+
+for(var i = 0; i < arr.length; i++) {
+    console.log("Element " + i + ": " + arr[i]);
 }
