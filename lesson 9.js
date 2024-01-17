aa84fec6df72d7d1052f017b83df6e06f37547e0
Afpa <alex.nesvit2000@gmail.com>
2024-01-17 13:47:31 +0100

    lesson 9 ok

--- lesson 9.js
+++ lesson 9.js
@@ -3,7 +3,20 @@
 alert("La méteo est exellente.");
 
 */
+/*
 var data = confirm("Recrutez-moi");
 if (data) {
     alert("Vous avez gagnier!");
+}*/
+/*
+var data = prompt("Quelle age vous avez");
+console.log(data);
+*/
+
+var person = null;
+if(confirm("Vous etes sur?")) {
+    person = prompt("Votre nom");
+    alert("Bonjour, " + person);
+} else {
+    alert("Vous aves annulé");
 }
