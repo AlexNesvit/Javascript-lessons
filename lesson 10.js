f384ac525fb00bcad29600772cce7c24b81d9406
Afpa <alex.nesvit2000@gmail.com>
2024-01-19 10:51:23 +0100

    lesson 10 v2

--- lesson 10.js
+++ lesson 10.js
@@ -8,7 +8,7 @@ function info() {
 info();
 
 */
-
+/*
 function info(word) {
     console.log(word + "!");
 
@@ -17,7 +17,23 @@ function info(word) {
 function summa(a, b) {
     var res = a + b;
     console.log(res);
+    info(res);
 }
 
 summa(5, 7);
 
+*/
+
+function summma(arr) {
+    var sum = 0;
+
+    for(var i = 0; i < arr.length; i++) 
+        sum += arr[i];
+
+        console.log(sum);
+}
+
+var array = [6, 8, 1];
+
+
+summma(array);
