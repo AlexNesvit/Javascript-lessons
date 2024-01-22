cf118ee4d1ebed3654df7a713346bfe7774bfff3
Afpa <alex.nesvit2000@gmail.com>
2024-01-22 10:50:21 +0100

    lesson 11v3

--- lesson 11.js
+++ lesson 11.js
@@ -3,5 +3,8 @@ var counter = 0;
 function onClickButton(el) {
    counter++;
    el.innerHTML = "Vous avez appuer:" + counter;
+   el.style.background = "red";
+   el.style.color = "blue";
    
+   el.style.cssText = "border-radius: 5px; border: 0; font-size: 20px";
 }
