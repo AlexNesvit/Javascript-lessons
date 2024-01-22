c4d241dc1c5277eb976bfcdab9dc1665c0b0b912
Afpa <alex.nesvit2000@gmail.com>
2024-01-22 13:21:34 +0100

    lesson 12 v3

--- lesson 12.js
+++ lesson 12.js
@@ -7,3 +7,8 @@ text.style.backgroundColor = "blue";
 
 text.innerHTML = "New";
 
+let spans = document.getElementsByTagName("span");
+
+for (let i = 0; i < spans.length; i++) {
+    console.log(spans[i].innerHTML);
+}
