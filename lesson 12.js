30468c7b7469a88d3f1f917b9bed5ef481dc2ac7
Afpa <alex.nesvit2000@gmail.com>
2024-01-22 15:30:17 +0100

    lesson 12 html v2

--- lesson 12.js
+++ lesson 12.js
@@ -1,5 +1,5 @@
 
-
+/*
 var text = document.getElementById("text");
 
 text.style.color = "red";
@@ -13,4 +13,34 @@ let spans = document.getElementsByClassName("simple");
 
 for (let i = 0; i < spans.length; i++) {
     console.log(spans[i].innerHTML);
+}
+
+*/
+
+function checkForm(el) {
+
+    let name = el.name.value;
+    let pass = el.pass.value;
+    let repass = el.repass.value;
+    let state = el.state.value;
+
+    let fail = "";
+   
+    if(name == "" || pass == "" || state == "")
+        error = "Invalid";
+    else if(name.length <= 1 || name.length > 50)
+        error = "Name incorrect";
+    else if(pass != repass)
+        error = "Pass incorrect";
+    else if(pass.split("&").length > 1)
+        error = "Pass incorrect";
+
+    if(error != "") {
+        document.getElementById("error").innerHTML = error;
+
+        return false;
+    } else {
+        alert("Ok");
+        return true;
+    }
 }
