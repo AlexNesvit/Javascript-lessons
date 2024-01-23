a0005ab2e99e260eb49e004aac39d568b4a4656e
Afpa <alex.nesvit2000@gmail.com>
2024-01-23 13:24:41 +0100

    lesson 13 v1

--- lesson 13.js
+++ lesson 13.js
@@ -1,7 +1,20 @@
-
+/*
 
 setInterval(my_func, 1000);
 
+let counter = 0;
 function my_func() {
-    
+    counter++;  
+    console.log("Counter: " + counter);
 }
+*/
+
+/*
+let counter = 0;
+setInterval(function() {
+counter++;
+console.log("Counter: " + counter);
+
+}, 1000);
+*/
+
