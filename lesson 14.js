04cf188779c5202641512528facdda07913b7a4d
Afpa <alex.nesvit2000@gmail.com>
2024-01-23 14:39:57 +0100

    lesson 14 v3

--- lesson 14.js
+++ lesson 14.js
@@ -4,10 +4,12 @@ let date = new Date();
 console.log(date.getHours() + ':' + date.getMinutes());
 
 */
-
+/*
 let arr = [8, 90, 5, 7, 8, 9];
 
 //console.log(arr.join(""));
 
 console.log(arr.sort());
+*/
+
 
