7d4143dbb428420dcae02126defd9a6c5d6445de
Afpa <alex.nesvit2000@gmail.com>
2024-01-24 14:49:24 +0100

    JavaScript per

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 
-let myName;
+// let myName;
 
-myName = "Alex";
-console.log(myName);
+// myName = "Alex";
+
+// console.log(myName);
+
+// const herName = "Bogdan";
+
+// console.log(herName);
+
+// const objectA = {
+//     a: 10,
+//     b: true 
+// }
+// const copyOfA = objectA;
+
+// copyOfA.c = "abc";
+
+const myCity = {
+    city: "New York"
+}
+
+myCity.popular = true;
+console.log(myCity);
+
+myCity.country = "USA";
+console.log(myCity);
+
+myCity.city = "La Ciotat";
+console.log(myCity);
