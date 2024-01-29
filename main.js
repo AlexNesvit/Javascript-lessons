1fc7772134c425201f6238bd79155c72dbb6f9e0
Afpa <alex.nesvit2000@gmail.com>
2024-01-29 16:51:26 +0100

    8:53

--- main.js
+++ main.js
@@ -277,7 +277,15 @@
 //     console.log(letter)
 // }
 
-//----classe---8:40---
+//----class---8:40---
 
-
+class Comment {
+    constructor(text) {
+        this.text = text
+        this.votesQty = 0
+    }
+    upvote() {
+        this.votesQty += 1
+    }
+}
 
