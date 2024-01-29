e7c644d3e74aae73a0beeea4abfae667d46e4a4d
Afpa <alex.nesvit2000@gmail.com>
2024-01-29 11:23:53 +0100

    6:13

--- main.js
+++ main.js
@@ -187,8 +187,9 @@
 
 //-----forEach----6:06----
 
-const myArray = [1, 2, 3]
-console.log(myArray)
+// const myArray = [1, 2, 3]
+// console.log(myArray)
 
-myArray.forEach(el => console.log(el * 2))
-console.log(myArray)
+// myArray.forEach(el => console.log(el * 2))
+// console.log(myArray)
+
