25f1d04d118593f2978f825a6beeb8a62a44f8f7
Afpa <alex.nesvit2000@gmail.com>
2024-01-30 13:52:44 +0100

    10:17

--- main.js
+++ main.js
@@ -295,3 +295,9 @@
 
 //  });
 
+// fetch("https://jsonplaceholder.typicode.com/todos")
+
+//     .then((response => response.json())
+//     .then(json => console.log(json))
+//     .catch((error) => console.error(error))
+
